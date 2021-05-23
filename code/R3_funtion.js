84c99dd674455d8085db1301957226495643fde7
wilberpulido <pulidowilber@gmail.com>
2021-05-23 16:45:24 -0300

    Complete R3

--- code/R3_funtion.js
+++ code/R3_funtion.js
@@ -1,3 +1,9 @@
 const showModal=()=>{
-    document.getElementById("modal").style.display = "block"
+    document.getElementById("modal").style.display = "flex"
+    document.getElementById("videoArticle").play();
+}
+const hideModal=()=>{
+    document.getElementById("modal").style.display = "none"
+    document.getElementById("videoArticle").pause();
+
 }
